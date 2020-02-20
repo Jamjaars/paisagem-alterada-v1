8f6da4878e844d6db762376ebaf1ca0dd980b382
Gabriel Fleury <gabrielfleury95@gmail.com>
2020-02-20 04:30:00 -0300

    nodes for loop

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,7 @@
 <template>
   <div class="home">
-      <img class="home__brasil" src="../assets/mapa-brasil.png"/>
-      <section class="f1"></section>
-      <section class="c2"></section>
-      <section class="d2"></section>
-      <section class="e2"></section>
-      <section class="f2"></section>
-      <section class="h2"></section>
-      <section class="i2"></section>
-      <section class="b3"></section>
-      <section class="c3"></section>
-      <section class="d3"></section>
-      <section class="e3"></section>
-      <section class="f3"></section>
-      <section class="g3"></section>
-      <section class="h3"></section>
-      <section class="i3"></section>
-      <section class="j3"></section>
-      <section class="k3"></section>
-      <section class="l3"></section>
+      <img class="home__brasil" src="../assets/mapa-brasil.png" />
+      <section v-for="quadrados in quadrantes" :key="quadrados" :class="quadrados"></section>
   </div>
 </template>
 
@@ -29,12 +12,30 @@ export default {
   name: "Home",
   data () {
     return {
-    }
+      quadrantes: [
+        "f1",
+        "c2",
+        "d2",
+        "e2",
+        "f2",
+        "h2",
+        "i2",
+        "j2",
+        "b3",
+        "c3",
+        "d3",
+        "e3",
+        "f3",
+        "g3",
+        "h3",
+        "i3",
+        "j3",
+        "k3",
+        "l3"
+      ]
+    };
   },
   components: {
   }
 };
 </script>
-
-<style scoped>
-</style>
