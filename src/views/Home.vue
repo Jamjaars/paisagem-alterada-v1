93915052eb1dbe94e8edc7777914a835e6d8bec2
Gabriel Fleury <gabrielfleury95@gmail.com>
2020-02-20 03:23:44 -0300

    added css grid

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,24 @@
 <template>
   <div class="home">
-      <h1>Paisagem Alterada</h1>
+      <img class="home__brasil" src="../assets/mapa-brasil.png"/>
+      <section class="f1"></section>
+      <section class="c2"></section>
+      <section class="d2"></section>
+      <section class="e2"></section>
+      <section class="f2"></section>
+      <section class="h2"></section>
+      <section class="i2"></section>
+      <section class="b3"></section>
+      <section class="c3"></section>
+      <section class="d3"></section>
+      <section class="e3"></section>
+      <section class="f3"></section>
+      <section class="g3"></section>
+      <section class="h3"></section>
+      <section class="i3"></section>
+      <section class="j3"></section>
+      <section class="k3"></section>
+      <section class="l3"></section>
   </div>
 </template>
 
@@ -9,7 +27,14 @@
 
 export default {
   name: "Home",
+  data () {
+    return {
+    }
+  },
   components: {
   }
 };
 </script>
+
+<style scoped>
+</style>
