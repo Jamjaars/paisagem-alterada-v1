dee5cf92318e899925824f4cfbd35c188686ea61
Gabriel Fleury <gabrielfleury95@gmail.com>
2020-02-27 20:37:00 -0300

    header horizontal scroll

--- src/components/HeaderNav.vue
+++ src/components/HeaderNav.vue
@@ -1,20 +1,22 @@
 <template>
-<div class="header__wrap">
-  <header class="header--fixed">
-    <div class="header__nav">
-      <span class="header__title">paisagem-alterada-v1</span>
-      <router-link class="header__link" to="/about">About</router-link>
-      <router-link class="header__link" to="/">Home</router-link>
-    </div>
-  </header>
-</div>
+  <div class="header__wrap" id="div_menu">
+    <header class="header--fixed">
+      <div class="header__nav">
+        <span class="header__title">paisagem-alterada-v1</span>
+        <router-link class="header__link" to="/about">About</router-link>
+        <router-link class="header__link" to="/">Home</router-link>
+      </div>
+    </header>
+  </div>
 </template>
 
 <script>
+var div_menu = document.getElementById("div_menu");
+window.onscroll = function() {
+  if (div_menu)
+  div_menu.style.top = window.pageYOffset + "px";
+};
 export default {
   name: "HeaderNav"
 }
 </script>
-
-<style scoped>
-</style>
