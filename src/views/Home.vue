cb5d2f769cd8613ad81faa42be88888d498c727e
Gabriel Fleury <gabrielfleury95@gmail.com>
2020-02-28 03:42:21 -0300

    mobile header fixed

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,6 +12,10 @@
 window.onload = window.scroll(500,0);
 export default {
   name: "Home",
+  metaInfo: {
+    title: "Paisagem Alterada | Clara Varandas",
+    viewport: 'width=device-width,initial-scale=1,user-scalable=no'
+  },
   data () {
     return {
       mapa: [
