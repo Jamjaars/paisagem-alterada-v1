a8bcff99b6c4ded53d6e705e7cbf3738aa5e4383
Gabriel Fleury <gabrielfleury95@gmail.com>
2020-02-28 03:49:18 -0300

    pinch zoom

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,7 +14,7 @@ export default {
   name: "Home",
   metaInfo: {
     title: "Paisagem Alterada | Clara Varandas",
-    viewport: 'width=device-width,initial-scale=1,user-scalable=no'
+    viewport: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no'
   },
   data () {
     return {
